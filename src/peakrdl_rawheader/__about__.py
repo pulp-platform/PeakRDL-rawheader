3008befc17c6e09b3f3b974e086532187f45d5f6
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-12-06 14:20:52 +0100

    py: Move version into pyproject.toml

--- src/peakrdl_rawheader/__about__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright 2025 ETH Zurich and University of Bologna.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Author: Michael Rogenmoser <michaero@iis.ee.ethz.ch>
-
-version_info = (1, 0, 0)
-__version__ = ".".join([str(n) for n in version_info])
